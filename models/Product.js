89e366efd6367f81fa1b29fa1b4d08f194b3bc54
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2024-11-24 16:27:55 +0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String },
+  imageUrl: { type: String, required: true },  // Thêm trường imageUrl
   createdAt: { type: Date, default: Date.now },
 });
 
